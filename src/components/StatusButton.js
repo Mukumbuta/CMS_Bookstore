6725cfc4068dcabac229e59a487e5e137046dfe4
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-10 12:13:13 +0200

    add check status button

---
+++ src/components/StatusButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StatusButton = () => (
+  <div>
+    <button type="button" className="statusBtn">Check Status</button>
+  </div>
+);
+
+export default StatusButton;
