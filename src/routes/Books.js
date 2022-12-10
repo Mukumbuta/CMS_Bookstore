76c05f2ac440de2d9bf373625c82af580c7be12c
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-10 12:13:57 +0200

    enabled page routing functionality

---
+++ src/routes/Books.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BooksContainer from '../components/BooksContainer';
+
+const Books = () => (
+  <div>
+    <BooksContainer />
+  </div>
+);
+
+export default Books;
