5334d3c0ae9b74689c25ae7e531a0da80fd44858
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-17 10:55:29 +0200

    add check status functionality

--- src/components/StatusButton.js
+++ src/components/StatusButton.js
@@ -1,9 +1,26 @@
+import { PropTypes } from 'prop-types';
 import React from 'react';
 
-const StatusButton = () => (
-  <div>
-    <button type="button" className="statusBtn">Check Status</button>
-  </div>
-);
+const StatusButton = (props) => {
+  const { dispatch, action } = props;
+  return (
+    <div>
+      <button
+        type="button"
+        className="statusBtn"
+        onClick={() => dispatch(action())}
+      >
+        Check Status
+      </button>
+    </div>
+
+  );
+};
+
+StatusButton.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  action: PropTypes.func.isRequired,
+
+};
 
 export default StatusButton;
